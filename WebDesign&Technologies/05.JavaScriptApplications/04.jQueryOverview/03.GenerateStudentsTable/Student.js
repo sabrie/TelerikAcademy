2ae9305e34612ddc5b933e1c4fdb36bbe02c90b4
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-06-24 15:52:36 +0300

    Add 04.jQueryOverview

---
+++ WebDesign&Technologies/05.JavaScriptApplications/04.jQueryOverview/03.GenerateStudentsTable/Student.js
@@ -0,0 +1,26 @@
+﻿///*jshint asi: false*/
+
+'use strict';
+
+var Student = (function () {
+    function Student(firstName, lastName, grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+    return Student;
+}());
+
+Student.prototype = {
+    getName: function () {
+        return this.firstName + ' ' +
+            this.lastName;
+    },
+    getGrade: function () {
+        return this.grade;
+    },
+    introduce: function () {
+        return 'Name: ' + this.getName() + '; Grade: ' + this.getGrade();
+    }
+};
