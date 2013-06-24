24eb03a1723ac4fd9439fec40417ac632bd8dd2d
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-06-24 15:48:20 +0300

    Add 05.Events

---
+++ WebDesign&Technologies/04.JavaScriptPartTwo/05.Events/01.Carousel/source.js
@@ -0,0 +1,50 @@
+﻿var images = [
+    "images/Agassi_Federer.jpg",
+    "images/Amelie.jpg",
+    "images/Nadal.jpg",
+    "images/Sharapova.jpg"
+];
+
+var image = document.getElementById("images");
+var currentImageNumber = 0;
+
+function nextImage() {
+
+    if (currentImageNumber === images.length - 1) {
+        currentImageNumber = 0;
+    } else {
+        currentImageNumber++;
+    }
+
+    image.src = images[currentImageNumber];    
+}
+
+function previousImage() {
+
+    if (currentImageNumber === 0) {
+        currentImageNumber = images.length - 1;
+    } else {
+        currentImageNumber--;
+    }
+    image.src = images[currentImageNumber];    
+}
+
+var previousButton = document.getElementById('previous');
+
+previousButton.addEventListener('click', function (e) {
+    // IE keeps the event object in window.event
+    e = e || window.event;
+
+    e.preventDefault()
+    previousImage();
+}, false);
+
+var nextButton = document.getElementById('next');
+
+nextButton.addEventListener('click', function (e) {
+    // IE keeps the event object in window.event
+    e = e || window.event;
+
+    e.preventDefault()
+    nextImage();
+}, false);
---
+++ WebDesign&Technologies/04.JavaScriptPartTwo/05.Events/02.TODOList-AddDeleteShowHide/source.js
@@ -0,0 +1,60 @@
+﻿var list = document.getElementById('todo-list');
+var addButton = document.getElementById('add-btn');
+var deleteButton = document.getElementById('delete-btn');
+var showButton = document.getElementById('show-btn');
+var hideButton = document.getElementById('hide-btn');
+
+function add(value) {
+    var listItem = document.createElement('li');
+
+    listItem.innerHTML = value;
+    list.appendChild(listItem);
+}
+
+function deleteItem(itemValue) {
+    var listItems = document.getElementsByTagName('li');
+
+    for (var i = 0; i < listItems.length; i++) {
+        if (listItems[i].innerHTML === itemValue) {
+            list.removeChild(listItems[i]);
+        }
+    }
+}
+
+function showList() {
+    
+    list.style.visibility = 'visible';
+}
+
+function hideList() {
+
+    list.style.visibility = 'hidden';
+}
+
+addButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var inputValue = document.getElementById('input').value;
+
+    add(inputValue);
+});
+
+deleteButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var inputValue = document.getElementById('input').value;
+
+    deleteItem(inputValue);
+});
+
+showButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    showList();
+});
+
+hideButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    hideList();
+});
