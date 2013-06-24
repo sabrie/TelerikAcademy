4b73635c182ec809d12b03967e9940bbcb0b36e9
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-06-24 14:29:12 +0300

    Add 07.HighQualityMethods

---
+++ Programming/04.HighQualityCode/07.HighQualityMethods/Methods/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Methods
+{
+    class Student
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string OtherInfo { get; private set; }
+        public DateTime BirthDate { get; private set; }
+
+        public bool IsOlderThan(Student other)
+        {
+            return this.BirthDate > other.BirthDate;
+        }
+    }
+}
