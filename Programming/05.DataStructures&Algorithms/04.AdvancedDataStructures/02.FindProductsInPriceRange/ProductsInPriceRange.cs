0dc56193dc416173d77dcb03be8352cbe4c03696
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-06-24 14:52:28 +0300

    Add 04.AdvancedDataStructures

---
+++ Programming/05.DataStructures&Algorithms/04.AdvancedDataStructures/02.FindProductsInPriceRange/ProductsInPriceRange.cs
@@ -0,0 +1,71 @@
+﻿ /* Write a program to read a large collection of products (name + price) 
+ * and efficiently find the first 20 products in the price range [a…b]. 
+ * Test for 500 000 products and 10 000 price searches.
+ * Hint: you may use OrderedBag<T> and sub-ranges.*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+class ProductsInPriceRange
+{
+    static void Main()
+    {
+        StreamReader fileName = new StreamReader("../../products.txt");
+
+        OrderedMultiDictionary<decimal, string> productsByPrice = new OrderedMultiDictionary<decimal, string>(true);
+
+        using (fileName)
+        {
+            string line;
+
+            while ((line = fileName.ReadLine()) != null)
+            {
+                var tokens = line.Split(';');
+                decimal price = decimal.Parse(tokens[0]);
+                string product = tokens[1];
+
+                productsByPrice.Add(price, product);
+            }
+        }
+
+        Console.WriteLine("Search products in price range: ");
+        Console.Write("From price:");
+        decimal fromPrice = decimal.Parse(Console.ReadLine());
+        Console.Write("To price: ");
+        decimal toPrice = decimal.Parse(Console.ReadLine());
+
+        var productsInPriceRange = productsByPrice.Range(fromPrice, true, toPrice, true);
+
+        Console.Write("Number of products to be printed: ");
+        int numberOfProductsToPrint = int.Parse(Console.ReadLine());
+        
+        PrintGivenNumberOfProducts(numberOfProductsToPrint, productsInPriceRange);
+    }
+
+    private static void PrintGivenNumberOfProducts(int numberOfProductsToPrint, 
+        OrderedMultiDictionary<decimal, string>.View productsInPriceRange)
+    {
+        if (numberOfProductsToPrint < productsInPriceRange.Count)
+        {
+            Console.WriteLine("There are no products or there are less " +
+                "than {0} in the given price range.", numberOfProductsToPrint);
+        }
+
+        int counter = 0;
+        foreach (var product in productsInPriceRange)
+        {
+            if (counter == numberOfProductsToPrint)
+            {
+                break;
+            }
+            
+            Console.WriteLine(product.Key + " " + product.Value);
+            counter++;
+        }
+    }
+}
---
+++ Programming/05.DataStructures&Algorithms/04.AdvancedDataStructures/03.FindGivenWordsOccurencesInText/GivenWordsOccurencesInText.cs
@@ -0,0 +1,71 @@
+﻿/* Write a program that finds a set of words (e.g. 1000 words) in a 
+ * large text (e.g. 100 MB text file). Print how many times each 
+ * word occurs in the text.
+ * Hint: you may find a C# trie in Internet. */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class GivenWordsOccurencesInText
+{
+    static void Main()
+    {
+        // simple text for test
+        // StreamReader fileName = new StreamReader("../../simpleText.txt");
+
+        StreamReader fileName = new StreamReader("../../text.txt");
+        string text;
+        
+        using (fileName)
+        {
+            text = ReadText(fileName);
+        }
+
+        var words = GetWords(text);
+
+        // creates a new Trie instance
+        ITrie trie = TrieFactory.GetTrie();
+
+        foreach (var word in words)
+        {
+            trie.AddWord(word.ToString());
+        }
+
+        string[] searchedWords = { "trie", "tree", "structure", "implementation", "dictionary"};
+
+        // Prints how many times each word from the searched words occurs in the text
+        foreach (var word in searchedWords)
+        {
+            Console.Write(word + " -> ");
+            int wordOcurrences = trie.WordCount(word);
+            Console.WriteLine(wordOcurrences + " times");
+        }
+    }
+
+    private static string ReadText(StreamReader input)
+    {
+        StringBuilder result = new StringBuilder();
+
+        string line = input.ReadLine();
+
+        while (line != null)
+        {
+            result.AppendLine(line);
+            line = input.ReadLine();
+        }
+
+        return result.ToString();
+    }
+
+    private static MatchCollection GetWords(string text)
+    {
+        string textToLower = text.ToLower();
+        var words = Regex.Matches(textToLower, @"\w+");
+
+        return words;
+    }
+}
+
