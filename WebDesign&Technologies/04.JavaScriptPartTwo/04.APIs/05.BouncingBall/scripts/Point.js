01a5c5c1af014577acc2aec8cd99bacd2d8c62a8
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-06-24 15:47:23 +0300

    Add 04.APIs

---
+++ WebDesign&Technologies/04.JavaScriptPartTwo/04.APIs/05.BouncingBall/scripts/Point.js
@@ -0,0 +1,31 @@
+﻿var Point = (function () {
+    var Point = function (x, y) {
+        this.x = x;
+        this.y = y;
+    };
+
+    Point.add = function (point1, point2) {
+        return new Point(
+            point1.x + point2.x,
+            point1.y + point2.y
+        );
+    };
+
+    Point.prototype.multiply = function (number) {
+        return new Point(
+            this.x * number,
+            this.y * number
+        );
+    }
+
+    Point.prototype.toString = function () {
+        var string = "x: " + this.x + ", y: " + this.y;
+
+        return string;
+    }
+
+    return Point;
+}());
+
+//console.log(Point.add(new Point(10, 10), new Point(-20, -20)));
+//console.log(new Point(10, 10).multiply(3).toString());
---
+++ WebDesign&Technologies/04.JavaScriptPartTwo/04.APIs/05.BouncingBall/scripts/source.js
@@ -0,0 +1,50 @@
+﻿window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var width = canvas.width = 400;
+var height = canvas.height = 300;
+
+var radius = 10;
+
+ctx.fillStyle = "green";
+ctx.lineWidth = 5;
+ctx.fillRect(0, 0, width, height);
+ctx.strokeRect(0, 0, width, height);
+
+
+function drawCircle(point) {
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.arc(point.x + radius, point.y + radius, radius, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.closePath();
+};
+
+var position = new Point(0, 0);
+var direction = new Point(3, 3); // change the speed here
+
+function moveCircle() {
+    position = Point.add(position, direction);
+    drawCircle(position);
+
+    if (!(0 <= position.x && position.x + 2 * radius < width)) {
+        direction.x *= -1;
+    }
+
+    if (!(0 <= position.y && position.y + 2 * radius < height)) {
+        direction.y *= -1;
+    }
+
+    requestAnimationFrame(moveCircle);
+};
+
+moveCircle();
