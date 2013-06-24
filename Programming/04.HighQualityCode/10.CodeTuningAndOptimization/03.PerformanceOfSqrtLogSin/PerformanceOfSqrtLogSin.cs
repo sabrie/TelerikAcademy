8a899e654319e35130e9e6584797ae8d0e43261c
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-06-24 14:31:03 +0300

    Add 10.CodeTuningAndOptimization

---
+++ Programming/04.HighQualityCode/10.CodeTuningAndOptimization/03.PerformanceOfSqrtLogSin/PerformanceOfSqrtLogSin.cs
@@ -0,0 +1,133 @@
+﻿/*
+  3. Write a program to compare the performance of 
+ square root, natural logarithm, sinus for float, double and decimal values.
+ */
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+
+class PerformanceOfSqrtLogSin
+{
+    static readonly Stopwatch stopwatch = new Stopwatch();
+
+    static void DisplayExecutionTime(string title, Action action)
+    {
+        Console.Write("{0, -30}", title);
+        stopwatch.Restart();
+
+        action();
+
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.Elapsed);
+    }
+
+    static void Main()
+    {
+        // Checks performans of Square root for float, double, decimal
+        int sqrtOf = 10000;
+
+        Console.WriteLine("Square root of {0}", sqrtOf);
+        Console.WriteLine(new string('-', 50));
+
+        DisplayExecutionTime("float", () =>
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Math.Sqrt(10000.0f);                
+            }           
+        }
+        );
+
+        DisplayExecutionTime("double", () =>
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Math.Sqrt(10000.0);
+            }   
+        }
+        );
+
+        // We can not pass as parameter a decimal value to Math.Sqrt(); 
+        // DisplayExecutionTime("decimal", () =>
+        //{
+        //    for (int i = 0; i < 100000; i++)
+        //    {
+        //        Math.Sqrt(10000.0m);
+        //    }
+        //}
+        //);
+
+        Console.WriteLine();
+
+        // Checks performans of Natural Logarithm for float, double, decimal
+        int logarithmOf = 10000;
+
+        Console.WriteLine("Natural logarithm of ", logarithmOf);
+        Console.WriteLine(new string('-', 50));
+
+        DisplayExecutionTime("float", () =>
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Math.Log(10000.0f);
+            }   
+        }
+        );
+
+        DisplayExecutionTime("double", () =>
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Math.Log(10000.0);
+            } 
+        }
+        );
+
+        // We can not pass as parameter a decimal value to Math.Log(); 
+        // DisplayExecutionTime("decimal", () =>
+        //{
+        //    for (int i = 0; i < 100000; i++)
+        //    {
+        //        Math.Log(10000.0m);
+        //    }
+        //}
+        //);
+
+        Console.WriteLine();
+
+        // Checks performans of Sinus for float, double, decimal
+        int sinusOf = 10000;
+
+        Console.WriteLine("Sinus of ", sinusOf);
+        Console.WriteLine(new string('-', 50));
+
+        DisplayExecutionTime("float", () =>
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Math.Sin(10000.0f);
+            } 
+        }
+        );
+
+        DisplayExecutionTime("double", () =>
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Math.Sin(10000.0);
+            } 
+        }
+        );
+
+        // We can not pass as parameter a decimal value to Math.Sin();
+        // DisplayExecutionTime("decimal", () =>
+        //{
+        //    for (int i = 0; i < 100000; i++)
+        //    {
+        //        Math.Sin(10000.0m);
+        //    }
+        //}
+        //);       
+    }
+}
---
+++ Programming/04.HighQualityCode/10.CodeTuningAndOptimization/2.CompareArithmeticPerformance/ArithmeticPerformance.cs
@@ -0,0 +1,284 @@
+﻿/*
+ 2. Write a program to compare the performance of add, subtract, increment, multiply, 
+ divide for int, long, float, double and decimal values.
+ 
+*/
+
+using System;
+using System.Diagnostics;
+
+class ArithmeticPerformance
+{
+    static readonly Stopwatch stopwatch = new Stopwatch();
+
+    static void DisplayExecutionTime(string title, Action action)
+    {
+        Console.Write("{0, -30}", title);
+        stopwatch.Restart();
+
+        action();
+
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.Elapsed);
+    }
+
+    static void Main()
+    {
+        // Checks Add performance for int, long, float, double, decimal
+        // ------------------------
+        int sumFromZeroTo = 100000000;
+
+        Console.WriteLine("Sum numbers from 0 to {0}", sumFromZeroTo);
+        Console.WriteLine(new string('-', 50));
+        
+        DisplayExecutionTime("int", () =>
+        {
+            int sum = 0;
+
+            for (int i = 0; i < sumFromZeroTo; i++)
+            {
+                sum += i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("long", () =>
+            {
+                long sum = 0;
+
+                for (int i = 0; i < sumFromZeroTo; i++)
+                {
+                    sum += i;
+                }
+            }
+        );
+
+        DisplayExecutionTime("float", () =>
+        {
+            float sum = 0;
+
+            for (int i = 0; i < sumFromZeroTo; i++)
+            {
+                sum += i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("double", () =>
+        {
+            double sum = 0;
+
+            for (int i = 0; i < sumFromZeroTo; i++)
+            {
+                sum += i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("decimal", () =>
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < sumFromZeroTo; i++)
+            {
+                sum += i;
+            }
+        }
+        );
+
+        Console.WriteLine();
+
+        // Checks Subtraction performance for int, long, float, double, decimal
+        // ------------------------
+
+        int startSubtractFrom = 100000000;
+
+        Console.WriteLine("Subtract numbers from {0} to 0", startSubtractFrom);
+        Console.WriteLine(new string('-', 50));
+
+        DisplayExecutionTime("int", () =>
+        {
+            int subtractResult = 0;
+
+            for (int i = startSubtractFrom; i <= 0; i--)
+            {
+                subtractResult -= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("long", () =>
+        {
+            long subtractResult = 0;
+
+            for (int i = startSubtractFrom; i <= 0; i--)
+            {
+                subtractResult -= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("float", () =>
+        {
+            float subtractResult = 0;
+
+            for (int i = startSubtractFrom; i <= 0; i--)
+            {
+                subtractResult -= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("double", () =>
+        {
+            double subtractResult = 0;
+
+            for (int i = startSubtractFrom; i <= 0; i--)
+            {
+                subtractResult -= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("decimal", () =>
+        {
+            decimal subtractResult = 0;
+
+            for (int i = startSubtractFrom; i <= 0; i--)
+            {
+                subtractResult -= i;
+            }
+        }
+        );
+
+        Console.WriteLine();
+
+        // Checks Multiplication performance for int, long, float, double, decimal
+        // ----------------------------
+
+        int multiplyFromOneTo = 10;
+
+        Console.WriteLine("Multiply numbers from 0 to {0}", multiplyFromOneTo);
+        Console.WriteLine(new string('-', 50));
+
+        DisplayExecutionTime("int", () =>
+        {
+            int multiplyResult = 1;
+
+            for (int i = 1; i <= multiplyFromOneTo; i++)
+            {
+                multiplyResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("long", () =>
+        {
+            long multiplyResult = 1;
+
+            for (int i = 1; i <= multiplyFromOneTo; i++)
+            {
+                multiplyResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("float", () =>
+        {
+            float multiplyResult = 1;
+
+            for (int i = 1; i <= multiplyFromOneTo; i++)
+            {
+                multiplyResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("double", () =>
+        {
+            double multiplyResult = 1;
+
+            for (int i = 1; i <= multiplyFromOneTo; i++)
+            {
+                multiplyResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("decimal", () =>
+        {
+            decimal multiplyResult = 1;
+
+            for (int i = 1; i <= multiplyFromOneTo; i++)
+            {
+                multiplyResult *= i;
+            }
+        }
+        );
+
+        Console.WriteLine();
+
+        // Checks division performance for int, long, float, double, decimal
+        // ------------------------
+
+        int startDivideFrom = 10;
+
+        Console.WriteLine("Divide numbers from {0} to 0", startDivideFrom);
+        Console.WriteLine(new string('-', 50));
+
+        DisplayExecutionTime("int", () =>
+        {
+            int divideResult = 1;
+
+            for (int i = 1; i <= startDivideFrom; i++)
+            {
+                divideResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("long", () =>
+        {
+            long divideResult = 1;
+
+            for (int i = 1; i <= startDivideFrom; i++)
+            {
+                divideResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("float", () =>
+        {
+            float divideResult = 1;
+
+            for (int i = 1; i <= startDivideFrom; i++)
+            {
+                divideResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("double", () =>
+        {
+            double divideResult = 1;
+
+            for (int i = 1; i <= startDivideFrom; i++)
+            {
+                divideResult *= i;
+            }
+        }
+        );
+
+        DisplayExecutionTime("decimal", () =>
+        {
+            decimal divideResult = 1;
+
+            for (int i = 1; i <= startDivideFrom; i++)
+            {
+                divideResult *= i;
+            }
+        }
+        );
+    }
+}
