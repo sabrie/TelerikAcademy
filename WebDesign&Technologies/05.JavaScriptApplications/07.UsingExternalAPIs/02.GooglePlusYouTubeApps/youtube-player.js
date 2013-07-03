0c6e1fd060f8db87977c0faf8ae13b9109acd4a6
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-07-03 20:01:25 +0300

    Added 07.UsingExternalAPIs

---
+++ WebDesign&Technologies/05.JavaScriptApplications/07.UsingExternalAPIs/02.GooglePlusYouTubeApps/youtube-player.js
@@ -0,0 +1,55 @@
+﻿var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '315', //must be bigger than 200px
+        width: '560', //must be bigger than 200px
+        videoId: 'GeL3XkhPyTM', // the ID of the first video - http://www.youtube.com/watch?v=GeL3XkhPyTM
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+
+    console.log(player);
+}
+
+function onPlayerReady(event) {
+    event.target.pauseVideo();
+}
+
+document.getElementById('single-video').addEventListener('click', function () {
+    var video = document.getElementById('load-video').value;
+
+    //player.cueVideoById(video, 0, "large");
+    player.loadVideoById(video, 0, "large");
+}, false);
+
+document.getElementById('pause').addEventListener('click', function () {
+    player.pauseVideo();
+}, false);
+
+document.getElementById('play').addEventListener('click', function () {
+    player.playVideo();
+}, false);
+
+document.getElementById('mute').addEventListener('click', function () {
+    player.mute();
+}, false);
+
+document.getElementById('unmute').addEventListener('click', function () {
+    player.unMute();
+}, false);
+
+document.getElementById('load-playlist').addEventListener('click', function () {
+    var videoPlaylist = document.getElementById('playlist').value.split(',');
+
+    //player.cuePlaylist(videoPlaylist, 0, 0, "large");
+    player.loadPlaylist(videoPlaylist, 0, 0, "large");
+}, false);
+
+document.getElementById('previous').addEventListener('click', function () {
+    player.previousVideo();
+}, false);
+
+document.getElementById('next').addEventListener('click', function () {
+    player.nextVideo();
+}, false);
---
+++ WebDesign&Technologies/05.JavaScriptApplications/07.UsingExternalAPIs/03-06.FacebookApp/Scripts/facebook.js
@@ -0,0 +1,65 @@
+﻿/// <reference path="Scripts/jquery-2.0.2.js" />
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '468633536558792',
+        channelUrl: 'http://localhost:20511/channel.html',
+        status: true, // check login status
+        cookie: true, // enable cookies to allow the server to access the session
+        xfbml: true  // parse XFBML
+    });
+
+    FB.login(function (response) {
+        if (response.authResponse) {
+            getProfileInfo();
+            getFriends();
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, { scope: 'read_friendlists,user_photos,user_birthday' });
+};
+
+// Load the SDK asynchronously
+(function (d) {
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+}(document));
+
+
+function getProfileInfo() {
+    FB.api('/me', function (response) {
+        var holder = $("#profile-info");
+        var name = response.name;
+        var birthday = response.birthday;
+        var location = response.location.name;
+        var url = "https://graph.facebook.com/" + response.id + "/picture";
+        holder.append("<img src =" + url + "/>");
+        holder.append("<p> My name: " + name + "</p>");
+        holder.append("<p> My birthday: " + birthday + "</p>");
+        holder.append("<p> Location: " + location + "</p>");
+    });
+    $("#log").css("display", "none");
+}
+
+function getFriends() {
+    FB.api('/me/friends', function (response) {
+        var friendsHolder = $('#friends-holder');
+        friendsHolder.append("<p>Here are my Facebook friends</p>");
+        for (i = 0; i < response.data.length; i++) {
+            var friendPictureUrl = 'https://graph.facebook.com/' + response.data[i].id + '/picture';
+            var friendName = response.data[i].name;
+            friendsHolder.append("<img class='images' src=" + friendPictureUrl + " title=" + friendName + "/>");
+        }
+    });
+}
+
+function fbLogout() {
+    FB.logout(function (response) {
+        window.location.reload();
+    });
+}
+
+$("#logout").click(function () { fbLogout() });
