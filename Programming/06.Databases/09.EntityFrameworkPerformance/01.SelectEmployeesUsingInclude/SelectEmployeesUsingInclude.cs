4e3f78f9d4b0c77c6b66834026fd30acc8d00de2
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-07-20 18:00:57 +0300

    Added 09.EntityFrameworkPerformance

---
+++ Programming/06.Databases/09.EntityFrameworkPerformance/01.SelectEmployeesUsingInclude/SelectEmployeesUsingInclude.cs
@@ -0,0 +1,48 @@
+﻿/*
+ Task 01.
+ Using Entity Framework write a SQL query to select all employees from 
+ the Telerik Academy database and later print their name, department and 
+ town. Try the both variants: with and without .Include(…). Compare the 
+ number of executed SQL statements and the performance.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelerikAcademy.Data;
+
+class SelectEmployeesUsingInclude
+{
+    static void Main()
+    {
+        TelerikAcademyEntities context = new TelerikAcademyEntities();
+
+        using (context)
+        {
+            // Low performance if not using .Include(...);
+            // 339 SQL statements are executed
+            foreach (var employee in context.Employees)
+            {
+                Console.WriteLine("Employee name: {0} {1}; ", employee.FirstName, employee.LastName);
+                Console.Write("Department: {0}; ", employee.Department.Name);
+                Console.Write("Town: {0}", employee.Address.Town.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            // Better performance when using .Include();
+            // Only 1 SQL statement is executed 
+            foreach (var employee in context.Employees.Include("Department").Include("Address.Town"))
+            {
+                Console.WriteLine("Employee name: {0} {1}; ", employee.FirstName, employee.LastName);
+                Console.Write("Department: {0}; ", employee.Department.Name);
+                Console.Write("Town: {0}", employee.Address.Town.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ Programming/06.Databases/09.EntityFrameworkPerformance/02.ToListPreformance/ToListPreformance.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Task 02.
+ Using Entity Framework write a query that selects all employees from 
+ the Telerik Academy database, then invokes ToList(), then selects 
+ their addresses, then invokes ToList(), then selects their towns, 
+ then invokes ToList() and finally checks whether the town is "Sofia".
+ Rewrite the same in more optimized way and compare the performance.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelerikAcademy.Data;
+
+
+class ToListPreformance
+{
+    static void Main()
+    {
+        TelerikAcademyEntities context = new TelerikAcademyEntities();
+        
+        // Low performance
+        // SQL statements are executed each time we invoke ToList();
+        var allEmployeesUsingToList = context.Employees.ToList()
+            .Select(e => e.Address).ToList()
+            .Select(e => e.Town).ToList()
+            .Where(t => t.Name == "Sofia");
+
+        // Better performance
+        // SQL statement is executed only when we call 'foreach'
+        var allEmployeesWithoutToList = context.Employees
+           .Select(e => e.Address)
+           .Select(e => e.Town)
+           .Where(t => t.Name == "Sofia");
+
+        using (context)
+        {
+            // 646 SQL statements are executed 
+            foreach (var employee in allEmployeesUsingToList)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            // 1 SQL statement is executed
+            foreach (var employee in allEmployeesWithoutToList)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+    }
+}
+
