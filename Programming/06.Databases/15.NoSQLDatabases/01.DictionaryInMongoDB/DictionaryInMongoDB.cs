bf48904bd426e1d4d20307acd0b1df4eee05b230
Sabrie Nedzhip <sabrie.nedzhip@gmail.com>
2013-07-20 17:48:47 +0300

    Added NoSQLDatabases

---
+++ Programming/06.Databases/15.NoSQLDatabases/01.DictionaryInMongoDB/DictionaryInMongoDB.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Task 01.
+Write a simple "Dictionary" application in C# or JavaScript to perform the following in MongoDB:
+    Add a dictionary entry (word + translation)
+    List all words and their translations
+    Find the translation of given word
+The UI of the application is up to you (it could be Web-based, GUI or console-based).
+You may use MongoDB-as-a-Service@ MongoLab.
+You may install the "Official MongoDB C# Driver" from NuGet 
+or download it from its publisher: http://docs.mongodb.org/ecosystem/drivers/csharp/
+*/
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class DictionaryInMongoDB
+{
+    static void Main()
+    {
+        // You may download MongoDB from the following link http://www.mongodb.org/downloads
+        // 1. Create a folder named 'mongodb' in C:\ to store the MongoDB 'bin' folder
+        // 2. Create in C:\ an empty folder named 'data' 
+        // 3. Create in C:\data an empty folder named 'db'
+        // 4. Start the MongoDB server from mongod.exe file -> C:\mongodb\bin\mongod.exe
+        // 5. Then start the VS project to see the results on the Console
+
+        var mongoClient = new MongoClient("mongodb://localhost/");
+        var mongoServer = mongoClient.GetServer();
+        var dictionary = mongoServer.GetDatabase("DictionaryDB");
+
+        var enBgDictionary = dictionary.GetCollection("EnBg");
+
+        // -----------------------------------------------------------
+        // Adds words and their translations to our EnBg dictionary
+        // -----------------------------------------------------------
+        enBgDictionary.Insert(new BsonDocument { { "word", "tree" }, { "translation", "дърво" } });
+        enBgDictionary.Insert(new BsonDocument { { "word", "apple" }, { "translation", "ябълка" } });
+        enBgDictionary.Insert(new BsonDocument { { "word", "cherry" }, { "translation", "череша" } });
+        enBgDictionary.Insert(new BsonDocument { { "word", "strawberry" }, { "translation", "ягода" } });
+
+        // ----------------------------------------------------------
+        // List all words and their translations
+        // ----------------------------------------------------------
+        var allWordsAndTranslations = enBgDictionary.FindAll();
+
+        Console.WriteLine("Word -> Translation");
+        Console.WriteLine(new string('-', 30));
+        foreach (var item in allWordsAndTranslations)
+        {
+            string word = (string)item["word"];
+            string translation = (string)item["translation"];
+            Console.WriteLine("{0} -> {1}", word, translation);
+        }
+        Console.WriteLine();
+
+        // ----------------------------------------------------------
+        // Find the translation of given word
+        // ----------------------------------------------------------
+        string searchedWord = "tree";
+        QueryDocument query = new QueryDocument("word", searchedWord);
+        var wordTranslations = enBgDictionary.Find(query);
+
+        Console.Write("Translation of '{0}' is ", searchedWord);
+        foreach (var item in wordTranslations)
+        {
+            Console.WriteLine(item["translation"]);
+        }
+
+        // Removes all the data from EnBg Collection
+        // enBgDictionary.RemoveAll();
+    }
+}
+
---
+++ Programming/06.Databases/15.NoSQLDatabases/02.DictionaryInRedis/DictionaryInRedis.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Task 02.
+ Write a simple "Dictionary" application in C# or JavaScript to perform the following in Redis:
+    Add a dictionary entry (word + translation)
+    List all words and their translations
+    Find the translation of given word
+You may hold the "word + meaning pairs" in a hash (see http://redis.io/commands#hash)
+See the HSET, HKEYS and HGET commands
+You may use a local Redis instance or register for a free "Redis To Go" account at https://redistogo.com.
+You may download the client libraries for your favorite programming language from http://redis.io/clients 
+or use the "ServiceStack.Redis" C# client from the NuGet package manager.
+
+ */
+
+using Sider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class DictionaryInRedis
+{
+    static void Main(string[] args)
+    {
+        // You may download Redis for Windows from https://github.com/MSOpenTech/redis/tree/2.6/bin/release
+        // Start the Redis server by double-click on redis-server.exe
+        // Start the Redis client by double-click on the redis-cli.exe
+        // Then start the VS project to see the result on the Console
+
+        using (var redisClient = new RedisClient())
+        {
+            // -----------------------------------------------------------
+            // Adds words and their translations to our EnBg dictionary
+            // ----------------------------------------------------------
+            redisClient.HSet("EnBg", "tree", "дърво");
+            redisClient.HSet("EnBg", "apple", "ябълка");
+            redisClient.HSet("EnBg", "strawberry", "ягода");
+            redisClient.HSet("EnBg", "blueberry", "боровинка");
+            redisClient.HSet("EnBg", "peach", "праскова");
+            redisClient.HSet("EnBg", "cherry", "череша");
+            redisClient.HSet("EnBg", "grape", "грозде");
+
+            // ---------------------------------------
+            // List all words and their translations
+            // ----------------------------------------
+            string[] allWords = redisClient.HKeys("EnBg");
+
+            Console.WriteLine("Word -> Translation");
+            Console.WriteLine(new string('-', 30));
+            for (int i = 0; i < allWords.Length; i++)
+            {
+                Console.WriteLine(allWords[i] + " -> " + redisClient.HGet("EnBg", allWords[i]));
+            }
+            Console.WriteLine();
+
+            // ----------------------------------------
+            // Find the translation of given word
+            // ----------------------------------------
+            Console.WriteLine("Find translation of a word");
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("Translation of 'tree' is {0}", redisClient.HGet("EnBg", "tree"));
+            Console.WriteLine("Translation of 'apple' is {0}", redisClient.HGet("EnBg", "apple"));
+            Console.WriteLine("Translation of 'cherry' is {0}", redisClient.HGet("EnBg", "cherry"));
+        }
+    }
+}
+
